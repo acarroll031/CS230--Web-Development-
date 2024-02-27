173e2c2683b708dce2efb0bf09647a573a829c7b
acarroll031 <16acarro@gmail.com>
2024-02-27 20:47:36 +0000

    Tidied code and added comments

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
+////// Increase Decrease Numbers ///////
 
+//Array to store nums for each level
 var counterStore = [0,0,0,0,0,0];
-var recommendedAdult = [0,0,2,3,4,6];
-var recommendedChild = [0,0,2,3,3,4]
-
-var fourplus = true;
 
 function increase(pos, id, pyramidlevel){
     counterStore[pos]++;
@@ -15,6 +13,12 @@ function decrease(pos, id){
     if(counterStore[pos] < 0) counterStore[pos] = 0;
     document.getElementById(id).innerText = counterStore[pos];
 }
+
+////// Change colour of numbers depending on recommendations //////
+
+//2 Arrays to store recommended values for adults and children
+var recommendedAdult = [0,0,2,3,4,6];
+var recommendedChild = [0,0,2,3,3,4];
 
 function checkRecommended(pos, id){
 
@@ -28,22 +32,26 @@ function checkRecommended(pos, id){
 
     if(id == 'l1') return false; //If in top level of pyramid, ignore
 
-    if(count == rec){
+    if(count == rec){ //If num items same as recommended, border green
         document.getElementById(id).style.borderColor = 'green';
     }
-    else if(count == rec+1 || count == rec-1){
+    else if(count == rec+1 || count == rec-1){ //If num items +-1 recommended, border yellow
         document.getElementById(id).style.borderColor = 'yellow';
     }
-    else{
+    else{ //If num items >+-1 recommended, border red
         document.getElementById(id).style.borderColor = 'red';
     }
 
 }
 
+////// Set the age from the dropdown //////
+
+//Boolean to keep track of if 4+ is selected
+var fourplus = true;
+
 function setAge(){
     var select = document.querySelector('#age');
     var ageRange = select.value;
-    console.log(ageRange);
 
     if(ageRange == 'onetofour'){
         fourplus = false;
@@ -51,19 +59,19 @@ function setAge(){
     else {
         fourplus = true;
     }
-
-    console.log(fourplus);
 }
 
+////// Check if date inputted is todays date //////
+
+//Create a date object for todays date
 const todaysDate = new Date();
+//Format object to a string that is the same format as the input string from the date input field
 const formattedTodaysDate = todaysDate.toISOString().substring(0,10);
 
 function setDate(){
     var select = document.querySelector('#date');
     var date = select.value;
-    console.log(date);
 
-    console.log(formattedTodaysDate);
     if(date != formattedTodaysDate){
         alert('Not Todays date!');
     }
